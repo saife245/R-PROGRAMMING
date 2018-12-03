a428be911017789275baed013007845766264625
MD SAIF UDDIN <41201124+saife245@users.noreply.github.com>
2018-12-03 19:02:21 +0530

    R PROGRAM

---
+++ vector_opertion.R
@@ -0,0 +1,16 @@
+#vector can be acess as array but in R index starting from 1
+vect  = c(1, 2, 3, 4, 5)
+print(vect[5])
+
+#it's means remove the given index elemnt and print all content of vect
+print(vect[-5])
+
+#printing the limited number of vector
+print(vect[1:3])
+
+#creating the second vector
+vect2 = c(9, 10, 11, 12, 13)
+#adding the two vector
+print(vect + vect2)
+
+
